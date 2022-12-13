21b13a5e12e3432602355f2098e6becf92029896
raisurak <raisurak54@gmail.com>
2022-12-13 02:00:54 -0500

    This is my demo if-else to push the project

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 20;
+        int y = 13;
+        if (y < x) {
+            System.out.println("x is greater than y");
+        }
+    }}
